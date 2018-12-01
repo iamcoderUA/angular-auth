771457c4649f01916307a34ca185c673b203f756
Stanislav Mikhniev <stanislav.mikhniev@gmail.com>
2018-12-02 00:23:57 +0200

    feat(auth): add children routes

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -4,7 +4,20 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth.component';
 
 export const authRoutes: Routes = [
-  { path: '', component: AuthComponent },
+  {
+    path: '',
+    component: AuthComponent,
+    children: [
+      {
+        path: 'login',
+        loadChildren: './login/login.module#LoginModule',
+      },
+      {
+        path: 'register',
+        loadChildren: './register/register.module#RegisterModule',
+      },
+    ],
+  },
 ];
 
 @NgModule({

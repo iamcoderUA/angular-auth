e2f334cf428850eb00d8067c9835936fb2ad82b0
Stanislav Mikhniev <stanislav.mikhniev@gmail.com>
2018-12-01 21:53:58 +0200

    feat(app): add logic for displaying loader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { LoaderService } from './core/services/loader.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-auth';
+export class AppComponent implements OnInit {
+
+  isLoaderShown$: Observable<boolean>;
+
+  constructor(
+    private loaderService: LoaderService
+  ) {
+  }
+
+  ngOnInit() {
+    this.isLoaderShown$ = this.loaderService.isLoaderShown$;
+  }
 }

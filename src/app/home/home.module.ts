c335e9687ead4ebd3187aa28633e89daca311d11
Stanislav Mikhniev <stanislav.mikhniev@gmail.com>
2018-12-01 20:23:54 +0200

    feat(home): create home-module

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+
+import { HomeRoutingModule } from './home.routing';
+
+import { HomeComponent } from './home.component';
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+  ],
+  imports: [
+    HomeRoutingModule
+  ],
+})
+export class HomeModule {
+}

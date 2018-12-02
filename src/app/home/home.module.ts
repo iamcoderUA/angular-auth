6246c8f8f35291f758a6309946f39d41bbf729e6
Stanislav Mikhniev <stanislav.mikhniev@gmail.com>
2018-12-02 21:24:04 +0200

    refactor(home): update naming

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 
-import { HomeRoutingModule } from './home.routing';
+import { HomeRoutingModule } from './home-routing.module';
 
 import { HomeComponent } from './home.component';
 
